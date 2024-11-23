61746e6b90d31d8d4a00af95e3aa77e7b73c6615
meego47 <meego47@users.noreply.github.com>
2024-11-23 15:08:32 +0000

    Fix styling

--- tests/Commands/DummyCommand.php
+++ tests/Commands/DummyCommand.php
@@ -7,6 +7,7 @@ use Illuminate\Console\Command;
 class DummyCommand extends Command
 {
     protected $signature = 'dummy:command';
+
     protected $description = 'A dummy command for testing purposes.';
 
     public function handle()
